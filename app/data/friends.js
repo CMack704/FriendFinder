fcc3a0417b71030931acfb7cc4b83fc1958d89be
Cmack704 <craigmack704@gmail.com>
2019-08-07 23:18:08 -0400

    started server.js, installed npm express and path, created package.json and .gitignore

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,57 +2,65 @@
     {
         "name": "Tony Stark",
         "photo": "../public/imgs/ironman.jfif",
-        "question1": 5,
-        "question2": 2,
-        "question3": 4,
-        "question4": 1,
-        "question5": 3,
-        "question6": 5,
-        "question7": 5,
-        "question8": 1,
-        "question9": 1,
-        "question10": 4
+        "scores": [
+            5,
+            2,
+            4,
+            1,
+            3,
+            5,
+            5,
+            1,
+            1,
+            4
+        ]
     },
     {
         "name": "Bruce Banner",
         "photo": "../public/imgs/hulk.jfif",
-        "question1": 5,
-        "question2": 3,
-        "question3": 1,
-        "question4": 4,
-        "question5": 2,
-        "question6": 4,
-        "question7": 3,
-        "question8": 4,
-        "question9": 4,
-        "question10": 2
+        "scores": [
+            5,
+            3,
+            1,
+            4,
+            2,
+            4,
+            3,
+            4,
+            4,
+            2
+        ]
     },
     {
         "name": "Jessica Jones",
         "photo": "../public/imgs/jones.jfif",
-        "question1": 3,
-        "question2": 5,
-        "question3": 4,
-        "question4": 2,
-        "question5": 1,
-        "question6": 5,
-        "question7": 5,
-        "question8": 4,
-        "question9": 1,
-        "question10": 1
+        "scores": [
+            3,
+            5,
+            4,
+            2,
+            1,
+            5,
+            5,
+            4,
+            1,
+            1
+        ]
     },
     {
         "name": "Jean Grey",
         "photo": "../public/imgs/jean.jfif",
-        "question1": 4,
-        "question2": 2,
-        "question3": 2,
-        "question4": 4,
-        "question5": 3,
-        "question6": 1,
-        "question7": 3,
-        "question8": 3,
-        "question9": 3,
-        "question10": 4
+        "scores": [
+            4,
+            2,
+            2,
+            4,
+            3,
+            1,
+            3,
+            3,
+            3,
+            4
+        ]
     }
 ]
