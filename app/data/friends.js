7891e62c880648447a3505ddbf1f3bf6d7ef4a26
Cmack704 <craigmack704@gmail.com>
2019-08-07 21:40:06 -0400

    added friends to friends.js and edited html more

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+[
+    {
+        "name": "Tony Stark",
+        "photo": "../public/imgs/ironman.jfif",
+        "question1": 5,
+        "question2": 2,
+        "question3": 4,
+        "question4": 1,
+        "question5": 3,
+        "question6": 5,
+        "question7": 5,
+        "question8": 1,
+        "question9": 1,
+        "question10": 4
+    },
+    {
+        "name": "Bruce Banner",
+        "photo": "../public/imgs/hulk.jfif",
+        "question1": 5,
+        "question2": 3,
+        "question3": 1,
+        "question4": 4,
+        "question5": 2,
+        "question6": 4,
+        "question7": 3,
+        "question8": 4,
+        "question9": 4,
+        "question10": 2
+    },
+    {
+        "name": "Jessica Jones",
+        "photo": "../public/imgs/jones.jfif",
+        "question1": 3,
+        "question2": 5,
+        "question3": 4,
+        "question4": 2,
+        "question5": 1,
+        "question6": 5,
+        "question7": 5,
+        "question8": 4,
+        "question9": 1,
+        "question10": 1
+    },
+    {
+        "name": "Jean Grey",
+        "photo": "../public/imgs/jean.jfif",
+        "question1": 4,
+        "question2": 2,
+        "question3": 2,
+        "question4": 4,
+        "question5": 3,
+        "question6": 1,
+        "question7": 3,
+        "question8": 3,
+        "question9": 3,
+        "question10": 4
+    }
+]
